49c790e03e424ba7ee793004ab6571b641c5d0a9
iannico322 <iannicocaulin@gmail.com>
2023-12-01 13:35:45 +0800

    hehe

---
+++ resources/js/main.js
@@ -0,0 +1,19 @@
+$(window).on('load',function(){
+    $(".loading").fadeOut(2000);
+    
+    $(".container").fadein(1000);
+})
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach((el) => observer.observe(el));
